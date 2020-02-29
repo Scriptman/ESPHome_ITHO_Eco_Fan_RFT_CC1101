bdf27cab29567daecf609f9768653c9dbdc817a9
M. Lamers <m.lamers@live.nl>
2020-02-29 16:24:22 +0100

    Updated for using on my old eco-fan 2 model

--- IthoCC1101.cpp
+++ IthoCC1101.cpp
@@ -286,13 +286,13 @@ void IthoCC1101::initReceive()
 	writeRegister(CC1101_IOCFG2 ,0x00);			//Assert when RX FIFO is filled or above the RX FIFO threshold. Deassert when (0x00): RX FIFO is drained below threshold, or (0x01): deassert when RX FIFO is empty.
 	writeRegister(CC1101_FSCTRL1 ,0x06);
 	writeRegister(CC1101_FSCTRL0 ,0x00);
-	writeRegister(CC1101_MDMCFG4 ,0xE8);
-	writeRegister(CC1101_MDMCFG3 ,0x43);
+	writeRegister(CC1101_MDMCFG4 ,0x5A);                    // For newer models 0xE8 (> 2011))
+	writeRegister(CC1101_MDMCFG3 ,0x83);                    // For newer models 0x43 (> 2011))
 	writeRegister(CC1101_MDMCFG2 ,0x00);		//Enable digital DC blocking filter before demodulator, 2-FSK, Disable Manchester encoding/decoding, No preamble/sync
 	writeRegister(CC1101_MDMCFG1 ,0x22);		//Disable FEC
 	writeRegister(CC1101_MDMCFG0 ,0xF8);
 	writeRegister(CC1101_CHANNR ,0x00);
-	writeRegister(CC1101_DEVIATN ,0x40);
+	writeRegister(CC1101_DEVIATN ,0x50);                    // For newer models 0x40 (>2011))
 	writeRegister(CC1101_FREND1 ,0x56);
 	writeRegister(CC1101_FREND0 ,0x17);
 	writeRegister(CC1101_MCSM0 ,0x18);			//no auto calibrate
@@ -301,11 +301,10 @@ void IthoCC1101::initReceive()
 	writeRegister(CC1101_AGCCTRL2 ,0x43);
 	writeRegister(CC1101_AGCCTRL1 ,0x40);
 	writeRegister(CC1101_AGCCTRL0 ,0x91);
-//	writeRegister(CC1101_FSCAL3 ,0xA9);
-	writeRegister(CC1101_FSCAL3 ,0xE9);
+	writeRegister(CC1101_FSCAL3 ,0xA9);                    // For newer models 0xE9 (> 2011))
 	writeRegister(CC1101_FSCAL2 ,0x2A);
 	writeRegister(CC1101_FSCAL1 ,0x00);
-	writeRegister(CC1101_FSCAL0 ,0x1F);
+	writeRegister(CC1101_FSCAL0 ,0x11);                    // For newer models 0x1F (> 2011))
 	writeRegister(CC1101_FSTEST ,0x59);
 	writeRegister(CC1101_TEST2 ,0x81);
 	writeRegister(CC1101_TEST1 ,0x35);
@@ -315,6 +314,7 @@ void IthoCC1101::initReceive()
 	writeRegister(CC1101_ADDR ,0x00);
 	writeRegister(CC1101_PKTLEN ,0xFF);
 	writeRegister(CC1101_TEST0 ,0x09);
+	writeRegister(CC1101_FSCAL2 ,0x00);                    // For newer models remove this line (> 2011))
 
 	writeCommand(CC1101_SCAL);
 
